a1bbd954e6cf92b1c2cda88761ecd6ac948c7971
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-10-24 20:17:13 +0200

    Added printf from a struct "string".

--- 03strings.cpp
+++ 03strings.cpp
@@ -1,9 +1,15 @@
+#include <stdio.h> //comes later
 #include <iostream>
 using namespace std;
 
+struct person{ //comes later
+    char * name; //printf only works if this isnt a string?
+    int age;
+};
+
 int main () {
-    string sometext="This is a new text"; //Declaring a string
-    string sometext2="+This!"; //String concat
+    string sometext="This is a new te"; //Declaring a string
+    string sometext2="xt!"; //String concat
 
     cout << "Initial value of sometext is: " << sometext+sometext2 << endl;  //Writing the initial text to the console.
     //Note: that endl isn't needed to show the value on the console, but adds a new line at the end
@@ -11,6 +17,14 @@ int main () {
     getline(cin, sometext);  //Reading textinput from the keyboard
 
     cout << "You entered: " << sometext << endl;  //Writing the given text back to the console.
+
+
+    //dont care about the following, just check the terminal values
+    person p1;
+    p1.name = (char *)"John", p1.age = 27;
+    string test = p1.name;
+    cout << test << endl;
+    printf("%s\n", p1.name); // should print "John"
 
     return 0;
 }
