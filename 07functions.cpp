d1750e0330c4919b0e28e28d09f8411fdb01a820
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-10-30 21:51:03 +0100

    Function Prototypes

--- 07functions.cpp
+++ 07functions.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int squareNumbers(int x);
+void helloThere(string name);
+void sum(int a, int b, int c);
+
+int main () { //Works now up here because of functionPrototypes
+    int a = 1;
+    int b = 4;
+    int c = 3;
+    int input = 9;
+    cout << squareNumbers(input) << endl;
+    helloThere("test");
+    sum(1,2,3);
+    return 0;
+}
+
 int squareNumbers(int x){ // Declares function "squareNumbers" that takes in parameter of x.
     int y=x*x; //creates int variable equating to x squared
     return y; //returns the value of y when the function is called
@@ -13,15 +28,4 @@ void helloThere(string name){//void means this function doesn't return anything
 //a function that prints out the sum of the given variables, a, b, and c. Below is the given code.
 void sum(int a, int b, int c) {
     cout << a+b+c << endl;
-}
-
-int main () {
-    int a = 1;
-    int b = 4;
-    int c = 3;
-    int input = 9;
-    cout << squareNumbers(input) << endl;
-    helloThere("test");
-    sum(1,2,3);
-    return 0;
 }
