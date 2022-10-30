e652851890938dfac54f789b0aa7b62c7dc291fc
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-10-30 22:31:53 +0100

    Did recursion

--- 12recursion.cpp
+++ 12recursion.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
-//TODO
+//TODO: You stopped here!
+
+char text[] = {'H','a','l','l','o','!',};
+
+void rekursion(char * input, int len, int now = 0);
 
 int main () {
+    rekursion(text, 6);
     return 0;
+}
+
+void rekursion(char input[], int len, int now) { //Defaultparam from the FuncPrototype seems to work here!
+    if (len != 1) {
+        cout << input[now];
+        rekursion(input, len-1, now+1);
+    }
+    else {
+        cout << input[now];
+    }
 }
