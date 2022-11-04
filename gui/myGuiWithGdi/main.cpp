ec0574f01b8b8513b65805934347d2c2eebb1be8
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-11-04 20:16:32 +0100

    ds

--- gui/myGuiWithGdi/main.cpp
+++
@@ -1,7 +0,0 @@
-//https://philippegroarke.com/posts/2018/c++_ui_solutions/
-// TODO: Continue here with Nuklear, minimal and good for the ModMenu
-
-//I compiled a GUI with this steps:
-//Open Terminal "x86 Native Tool Command Prompt for VS 2022"
-//Navigate this folder with build.bat inside and run it
-//=> Executable GUI generated and intermediate .obj file
