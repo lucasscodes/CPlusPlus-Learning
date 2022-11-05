d780030f4ead5511d64467ee377f47c9964bcf3d
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-11-05 14:17:05 +0100

    dx3d11 ready!

---
+++ gui/dx3d11/nuklear_d3d11.hlsl
@@ -0,0 +1,38 @@
+/*High Level Shader Language (HLSL), a graphics shading language developed by Microsoft; 
+may be used for a geometry shader, a vertex shader, or a pixel shader; 
+used for programming how 3D objects appear in Direct3D games and programs.*/
+cbuffer buffer0 : register(b0)
+{
+  float4x4 ProjectionMatrix;
+};
+
+sampler sampler0 : register(s0);
+Texture2D<float4> texture0 : register(t0);
+
+struct VS_INPUT
+{
+  float2 pos : POSITION;
+  float4 col : COLOR0;
+  float2 uv  : TEXCOORD0;
+};
+
+struct PS_INPUT
+{
+  float4 pos : SV_POSITION;
+  float4 col : COLOR0;
+  float2 uv  : TEXCOORD0;
+};
+
+PS_INPUT vs(VS_INPUT input)
+{
+  PS_INPUT output;
+  output.pos = mul(ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));
+  output.col = input.col;
+  output.uv  = input.uv;
+  return output;
+}
+
+float4 ps(PS_INPUT input) : SV_Target
+{
+  return input.col * texture0.Sample(sampler0, input.uv);
+}
