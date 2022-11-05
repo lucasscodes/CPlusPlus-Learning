b6118f0853dbbcb7427817c92b1fbb5c76204466
Lucas Schmidt <lucas.schmidt-1@studium.uni-hamburg.de>
2022-11-05 13:49:17 +0100

    Got Dx3D9+11 working, now on Dx3D12!

--- gui/gdi/main.c
+++ gui/gdi/main.c
@@ -1,3 +1,11 @@
+//https://philippegroarke.com/posts/2018/c++_ui_solutions/
+// TODO: Continue here with Nuklear, minimal and good for the ModMenu
+
+//I compiled a GUI with this steps:
+//Open Terminal "x86 Native Tool Command Prompt for VS 2022"
+//Navigate this folder with build.bat inside and run it
+//=> Executable GUI generated and intermediate .obj file
+
 /* nuklear - 1.32.0 - public domain */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
